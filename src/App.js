b4606781a9a541601460811049c35f7ed21c6308
AhmedMaiez-am <ahmeddhiaeddine.maiez@esprit.tn>
2022-01-25 12:40:46 +0100

    workshop1

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const menu = (<div>
+    <ul id="nav">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">FAQ</a></li>
+    <li><a href="#">Contact</a></li>
+    </ul>
+    </div>
+    );
+  return (
+<div>
+          
+            {menu}
+            <div>
+              [Page content here]
+            </div>
+          </div>
+  );
+}
+
+export default App;
